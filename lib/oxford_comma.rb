b1f53f32aa7346e5ef54c4d8f6d32ab714d20b56
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-14 10:02:17 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
 def oxford_comma(array)
+  
 
+ joined_array = array.join 
+ joined_array = array.join(" and ")
+ 
+ if array.length == 3
+   
+   joined_array = array[0..1].join(", ")
+   
+   joined_array << ", and #{array[-1]}"
+elsif array.length > 3
+   new_array_for_last_array_element = array.last
+   array.pop
+   joined_array = array.join(", ")
+   joined_array << ", and #{new_array_for_last_array_element}"
+ else
+   joined_array
+  end 
+  joined_array
 end
+
+
